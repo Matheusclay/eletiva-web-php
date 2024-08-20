0387b1aaa614273b2086c54735439b0c7bfa6211
Matheus Henrique da Silva <matheeus.silva9@hotmail.com>
2024-08-19 22:10:23 -0300

    exercicios de POO concluido

---
+++ POO/livros.php
@@ -0,0 +1,48 @@
+<?php
+class Livro {
+    private $titulo;
+    private $autor;
+    private $ano;
+
+
+    public function __construct($titulo, $autor, $ano) {
+        $this->titulo = $titulo;
+        $this->autor = $autor;
+        $this->setAno($ano);
+    }
+
+    public function setTitulo($titulo) {
+        $this->titulo = $titulo;
+    }
+
+    public function getTitulo() {
+        return $this->titulo;
+    }
+
+    public function setAutor($autor) {
+        $this->autor = $autor;
+    }
+
+    public function getAutor() {
+        return $this->autor;
+    }
+
+    public function setAno($ano) {
+        if ($ano < 1900) {
+            $ano = 1900;
+        }
+
+        $this->ano = $ano;
+        
+    }
+
+    public function getAno() {
+        return $this->ano;
+    }
+
+}
+
+
+
+$obj = new Livro("O Senhor dos Anéis", "J. R. R. Tolkien", 1970);
+var_dump($obj);
